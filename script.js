e954cb515d03e803849ee8f6125bdfb1c3ce15a9
dooli1971039 <dooli9819@gmail.com>
2022-11-27 04:33:59 +0900

    🚧 work in progress

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-GitHubCalendar(".calendar", "dooli1971039", {
-    responsive: true,
-    tooltips: true,
-    global_stats: false,
-}).then(() => {
-    document.querySelector(".calendar").style.maxWidth = "900px";
-    // document.querySelector(".calendar").style.minHeight = "100px";
-    // document.getElementsByClassName("width-full")[0].style.display = "none";
-
-    let parentList = document.querySelectorAll("svg>g>g");
-    let childList = parentList[parentList.length - 1].childNodes;
-    let lastBlock = childList[childList.length - 2];
-    console.log(lastBlock.style);
-    console.log(lastBlock.dataset); // count, date, level
-});
-
-//https://github.com/users/dooli1971039/contributions
-
-// const url = "https://github.com/users/dooli1971039/contributions";
-// fetch(url)
-//     .then((respose) => respose.json)
-//     .then((data) => console.log(data));
-
 // let st = document.querySelector("style").innerHTML;
 // document.querySelector("style").innerHTML = `
 // :root {
@@ -31,3 +8,10 @@ GitHubCalendar(".calendar", "dooli1971039", {
 //     --color-calendar-graph-day-L4-bg:#33bbe4;
 // }
 // `;
+
+//https://github.com/users/dooli1971039/contributions
+
+const url = "https://github.com/users/dooli1971039/contributions";
+fetch(url)
+    .then((respose) => respose.json())
+    .then((data) => console.log(data));
