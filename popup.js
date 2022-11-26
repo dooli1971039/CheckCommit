d06d297dc5889a697f95410c4257da93cd6f5a26
dooli1971039 <dooli9819@gmail.com>
2022-11-27 02:10:04 +0900

    ✨ Reconstruct login part and create main part

--- popup.js
+++ popup.js
@@ -1,95 +1,94 @@
-const beforeLogin = document.querySelector("#before-login");
-const enterID = beforeLogin.querySelector("input");
-const warning = beforeLogin.querySelector("#warning");
-const afterLogin = document.querySelector("#after-login");
-const logoutBtn = document.querySelector("#logout");
+const part1 = document.querySelector("#part1");
+const inputID = document.querySelector("#input-id");
+const warning = document.querySelector("#warning");
+const loginBtn = document.querySelector("#login-button");
 
-let userID = null;
-let userName = null;
-let userUrl = null;
-const userGraphUrl = `https://ghchart.rshah.org/`;
+const part2 = document.querySelector("#part2");
+const comment = document.querySelector("#comment");
+const calendar = document.querySelector(".calendar");
+// const logoutBtn = document.querySelector("#logout");
 
-beforeLogin.addEventListener("submit", clickLogin);
-logoutBtn.addEventListener("click", clickLogout);
+loginBtn.addEventListener("click", clickLogin);
+calendar.addEventListener("click", openGithub);
 
 function clickLogin(event) {
     event.preventDefault();
-    userID = enterID.value;
-    enterID.value = ""; //입력창 비우기
-    getInformation(userID);
-}
+    //입력창에서 아이디 가져와서 체크
+    let id = inputID.value;
+    const checkUrl = `https://api.github.com/users/${id}`;
 
-function getInformation(userID) {
-    const url = `https://api.github.com/users/${userID}`;
-
-    fetch(url)
+    fetch(checkUrl)
         .then((response) => response.json())
         .then((data) => {
-            userName = data.name;
-            userUrl = data.html_url;
-
-            if (userName === null) {
-                // null이면 계정 없는 경우(warning msg 보여주기)
-                showTag(warning);
+            if (data.name == null) {
+                //아이디 잘못 입력 시, warning 메세지 띄우기
+                warning.classList.remove("hidden");
             } else {
-                //setting
-                setAfterLogin(userName, userID);
-                storeID(userID);
+                //아이디 제대로 입력 시
+                //관련 정보 localStorage에 저장 후, after-login 세팅
+                localStorage.setItem("userID", id);
+                setAfterLogin(id);
 
                 //화면 전환
-                showTag(afterLogin);
-                hideTag(warning, "hidden");
-                hideTag(beforeLogin, "none");
+                warning.classList.add("hidden");
+                part1.classList.add("none");
+                part2.classList.remove("none");
             }
+        })
+        .catch(() => {
+            //아이디 잘못 입력 시, warning 메세지 띄우기
+            warning.classList.remove("hidden");
         });
+    inputID.value = ""; //초기화
 }
 
-function clickLogout(event) {
-    //localStorage 삭제, 변수 초기화
-    localStorage.removeItem("userID");
-    localStorage.removeItem("userName");
-    userID = userName = userUrl = null;
+function setAfterLogin(id) {
+    GitHubCalendar(".calendar", id, {
+        responsive: true,
+        tooltips: false,
+        global_stats: false,
+    }).then(() => {
+        /* 필요 없는 요소 삭제 */
+        document.getElementsByClassName("width-full")[0].remove();
+        let textList = document.querySelectorAll("svg>g>text");
+        textList.forEach((item) => item.remove());
 
-    //afterLogin 하위에 추가한 태그 삭제
+        /* 위치 조정 */
+        document.querySelector(".calendar").style.minHeight = "70px";
+        let cal = document.querySelector("svg>g");
+        cal.attributes[0].value = "translate(0,15)";
+
+        /* 데이터 처리 및 comment 띄우기*/
+        let parentList = document.querySelectorAll("svg>g>g");
+        let childList = parentList[parentList.length - 1].childNodes;
+        console.log(childList);
+        let lastBlock = childList[childList.length - 2];
+        console.log(lastBlock.dataset); // count, date, level
+
+        let lastBlockDate = lastBlock.dataset.date;
+        let lastBlockCount = Number(lastBlock.dataset.count);
+        console.log(lastBlockDate, lastBlockCount);
+
+        if (lastBlockCount === 0) {
+            comment.innerHTML = "COMMIT NOW";
+        } else {
+            comment.innerHTML = `you did ${lastBlockCount} commits today`;
+        }
+    });
+}
+
+function openGithub() {
+    const openUrl = `https://github.com/${localStorage.getItem("userID")}`;
+    window.open(openUrl);
+}
+
+/*바로 실행**********************************************/
+let checkLocalHostID = localStorage.getItem("userID");
+if (checkLocalHostID) {
+    setAfterLogin(checkLocalHostID);
 
     //화면 전환
-    hideTag(afterLogin, "none");
-    showTag(beforeLogin);
+    warning.classList.add("hidden");
+    part1.classList.add("none");
+    part2.classList.remove("none");
 }
-
-function showTag(tag) {
-    tag.classList.remove("hidden");
-    tag.classList.remove("none");
-}
-
-function hideTag(tag, type) {
-    tag.classList.add(type);
-}
-
-function setAfterLogin(userName, userID) {
-    let greeting = document.createElement("h2");
-    greeting.innerText = `Hello ${userName}`;
-    afterLogin.appendChild(greeting);
-
-    let githubImg = document.createElement("img");
-    githubImg.src = userGraphUrl + userID;
-    afterLogin.appendChild(githubImg);
-}
-
-function storeID(userID) {
-    localStorage.setItem("userName", userName);
-    localStorage.setItem("userID", userID);
-}
-
-/***********************************************************************************/
-// //가장 먼저 아이디가 있는지 체크
-if (localStorage.getItem("userID")) {
-    setAfterLogin(localStorage.getItem("userName"), localStorage.getItem("userID"));
-
-    showTag(afterLogin);
-    hideTag(beforeLogin, "none");
-}
-/**
- * 캘린더 수정하기
- * 캘린더 색 변환은 어떻게 할 수 있을까나....
- */
