2fa18708ac5b086cae7186af5a264585cd32c895
dooli1971039 <dooli9819@gmail.com>
2022-11-27 14:29:54 +0900

    🚧 work in progress

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// let st = document.querySelector("style").innerHTML;
-// document.querySelector("style").innerHTML = `
-// :root {
-//     --color-calendar-graph-day-bg: #33bbe4;
-//     --color-calendar-graph-day-L1-bg:#33bbe4;
-//     --color-calendar-graph-day-L2-bg: #33bbe4;
-//     --color-calendar-graph-day-L3-bg: #33bbe4;
-//     --color-calendar-graph-day-L4-bg:#33bbe4;
-// }
-// `;
-
 //https://github.com/users/dooli1971039/contributions
 
-const url = "https://github.com/users/dooli1971039/contributions";
+const url = "https://api.github.com/users/dooli1971039";
 fetch(url)
     .then((respose) => respose.json())
     .then((data) => console.log(data));
+// https://api.github.com/users/dooli1971039/events
