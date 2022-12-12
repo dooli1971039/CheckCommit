392a847daa5a5bdea095160d470155efffdb890c
dooli1971039 <dooli9819@gmail.com>
2022-12-13 05:08:50 +0900

    ⚰️ Remove unnecessary code

--- script.js
+++
@@ -1,7 +0,0 @@
-//https://github.com/users/dooli1971039/contributions
-
-const url = "https://api.github.com/users/dooli1971039";
-fetch(url)
-    .then((respose) => respose.json())
-    .then((data) => console.log(data));
-// https://api.github.com/users/dooli1971039/events
